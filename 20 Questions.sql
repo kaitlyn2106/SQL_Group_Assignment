0e318869cef69a677851d27d2074e9066129df73
islandgeckos <57193839+islandgeckos@users.noreply.github.com>
2019-11-21 16:06:28 -0700

    Update 20 Questions.sql

--- 20 Questions.sql
+++ 20 Questions.sql
@@ -1,3 +1,6 @@
+-got all of them but one...
+
+
 #Q1 :)
 
 SELECT product_code, product_name, list_price, discount_percent
@@ -20,40 +23,43 @@ from products
 where list_price between 500 and 2000
 ORDER BY date_added DESC;
 
-#Q4
+#Q4 :)
 
-select product_name, list_price, discount_percent
-where
-sum(list_price*discount_percent) as discount_amount
-and
-sum(list_price-discount_amount) as discount_price
+select product_name, list_price, discount_percent,
+round((list_price*discount_percent),2) as discount_amount,
+round((list_price-discount_amount),2) as discount_price
 from products
-where round(discount_amount, 2)
-and round(discount_price, 2)
-limit 5
-ORDER BY discount_price desc;
+ORDER BY discount_price desc
+limit 5;
 
-#Q5
+#Q5 :)
 
-select item_id, item_price, discount_amount, quantity
-where sum(item_price*quantity) = price_total
-
+select item_id, item_price, discount_amount, quantity,
+sum(item_price*quantity) as price_total,
+sum(discount_amount*quanity) as discount_total,
+sum((itme_price-discount_amount)*quantity) as item_total
 from products
+where item_total > 500
+order by item_total desc;
+
+
 
 #Q6 :)
 
 select order_id, order_date, ship_date from orders
 where ship_date like null;
 
-#Q7
+#Q7 :)
 
-select today_unformated
-as today_formated where NOW(DD-Mon_YYYY):
+select now() as today_unformated, date_format(now(), '%d-%b-%Y') as today_formatted;
 
-#Q8 
-select price, tax_rate
-where sum(price*tax_rate) as tax_amount
-and sum(price+tax_amount) as total;
+#Q8 :)
+
+use my_guitar_shop;
+select 100 as price, .07 as tax_rate, sum(100*.07) as tax_amount,
+sum(100+(100*.07)) as total
+;
+
 
 #PART 3
 
@@ -87,11 +93,12 @@ quantity as 'item amount'
 from customers, orders, order_items, products
 order by last_name, order_date, product_name; 
 
-#Q5
-select product_name, list_price
-from products
-where distinct product_id
-where list_Price
+#Q5 :(
+select product_name, 'p1' as product_list, 'p2' as product_list
+from products as p1
+join products as p2 on
+product_id <> product_id
+and list_price = price_list
 order by product_name;
 
 #Q6 :)
@@ -100,13 +107,15 @@ select category_name, product_id
 from categories, products
 where product_id is null;
 
-#Q7
+#Q7 :)
  
-select concat_ws(ship_date) as 'ship_status',
-order_id, order_date
+select 'shipped' as ship_status, order_id, order_date
 from orders
-where ship_status not null is 'SHIPPED'
-and shup_status null is 'NOT SHIPPED'
+where ship_date is not null
+union
+select 'not shipped', order_id, order_date
+from orders
+where ship_date is null
 order by order_date;
 
 #PART 4
@@ -139,4 +148,3 @@ INSERT INTO products (product_id, category_id, product_code, product_name, descr
 update products
 set discount_percent = '35'
 where product_id = 11;
-
